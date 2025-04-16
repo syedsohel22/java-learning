0fad4503baf3f8370d5a2b3d4fd8cf985f994b84
sohel <mr.sohelsyed@gmail.com>
2025-04-16 23:23:40 +0530

    variable naming rules

--- java/Main.java
+++ java/Main.java
@@ -44,7 +44,14 @@ public class Main {
     String greeting = "Hello, " + name + "!"; // string concatenation
     System.out.println(greeting); // prints "Hello, John!"
     
-    
+// The general rules for naming variables are:
+
+// -  Names can contain letters, digits, underscores, and dollar signs
+// - Names must begin with a letter
+// - Names should start with a lowercase letter, and cannot contain whitespace
+// - Names can also begin with $ and _
+// - Names are case-sensitive ("myVar" and "myvar" are different variables)
+// - Reserved words (like Java keywords, such as int or boolean) cannot be used as names
     
   }
 }
